83784720c2ac229073004adf909db8c5fe2c82b3
ticze <ticze2598@gmail.com>
2018-10-03 10:50:08 -0500

    base de datos de visitantes en firebase

--- src/call-firebase.js
+++ src/call-firebase.js
@@ -1,10 +1,10 @@
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyD8GYZ97ggyaqvwJrLQqawi1EYKkdNPZpc",
-    authDomain: "you-work.firebaseapp.com",
-    databaseURL: "https://you-work.firebaseio.com",
-    projectId: "you-work",
-    storageBucket: "you-work.appspot.com",
-    messagingSenderId: "482327280285"
-  };
-  firebase.initializeApp(config);  
+  apiKey: "AIzaSyD8GYZ97ggyaqvwJrLQqawi1EYKkdNPZpc",
+  authDomain: "you-work.firebaseapp.com",
+  databaseURL: "https://you-work.firebaseio.com",
+  projectId: "you-work",
+  storageBucket: "you-work.appspot.com",
+  messagingSenderId: "482327280285"
+};
+firebase.initializeApp(config);  
---
+++ src/index.js
@@ -0,0 +1,20 @@
+const btnRegister = document.getElementById('btn-for');
+
+btnRegister.addEventListener('click', () => {
+  event.preventDefault();
+  const name = document.getElementById('nombres').value;
+  const dni = document.getElementById('dni').value;
+  const cellPhone = document.getElementById('celular').value;
+  const slcVisitante = document.getElementById('slc-vst').value;
+  const visitDate = new Date().toLocaleString();
+  console.log(name, dni, cellPhone,slcVisitante, visitDate)
+  
+  const admVisitantes = firebase.database().ref().child('visitante');
+  admVisitantes.push({
+    name: name,
+    dni: dni,
+    cell: cellPhone,
+    persona_a_visitar: slcVisitante,
+    fecha_de_visita: visitDate,
+  })
+})
