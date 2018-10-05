a7912b387fbc2e8445b3de73215f02781b302092
ticze <ticze2598@gmail.com>
2018-10-05 11:30:39 -0500

    login de administrador

---
+++ src/adm.js
@@ -0,0 +1,76 @@
+const registrar = () => {
+  const email = document.getElementById('email').value;
+  const contrasena = document.getElementById('contrasena').value;
+
+  firebase.auth().createUserWithEmailAndPassword(email, contrasena)
+      .catch(function (error) {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorMessage)
+      });
+
+}
+const ingresar = () => {
+  const email2 = document.getElementById('email2').value;
+  const contrasena2 = document.getElementById('contrasena2').value;
+  firebase.auth().signInWithEmailAndPassword(email2, contrasena2)
+    .catch(function (error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      console.log(errorMessage)
+    });
+
+}
+const observador = () => {
+  firebase.auth().onAuthStateChanged(function (user) {
+    if (user) {
+      dtVisitante();
+      // User is signed in.
+      var displayName = user.displayName;
+      var email = user.email;
+      var emailVerified = user.emailVerified;
+      var photoURL = user.photoURL;
+      var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      var providerData = user.providerData;
+      // ...
+    } else {
+      // User is signed out.
+      alert('No existe usuario')
+    }
+  });
+}
+observador()
+
+const dtVisitante = () => {
+  const contenido = document.getElementById('contenido');
+  let ref = firebase.database().ref('/visitante');
+    ref.once('value', (data) => {
+      data.forEach(visitante => {
+        let visitor = visitante.key,
+          visitanteDatos = visitante.val();
+          contenido.innerHTML = ` 
+          <thead>
+          <tr>
+              <th>Nombre</th>
+              <th>Núm. de Identidad</th>
+              <th>Celular</th>
+              <th>Persona Visitada</th>
+              <th>Hora/Fecha</th>
+          </tr>
+        </thead>
+      
+        <tbody>
+          <tr>
+            <td>${visitanteDatos.name}</td>
+            <td>${visitanteDatos.dni}</td>
+            <td>${visitanteDatos.cell}</td>
+            <td>${visitanteDatos.persona_a_visitar}</td>
+            <td>${visitanteDatos.fecha_de_visita}</td>
+          </tr>
+        </tbody>
+        `
+      })
+    });
+}
